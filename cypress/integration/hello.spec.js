3ec0c31ae8f01b8cd94fde215927b6701fa9d924
breslavsky <breslavsky.anton@gmail.com>
2022-04-10 20:06:17 +0200

    Remove chrome tests

---
+++ cypress/integration/hello.spec.js
@@ -0,0 +1,17 @@
+describe('Привет Cypress', () => {
+
+    it('открыть сайт', () => {
+        cy.visit('https://www.cypress.io/');
+    });
+
+    it('подписаться на рассылку', () => {
+        cy.get('#input-footer-email')
+            .click()
+            .type('test@test.ru');
+
+        cy.get('.styled__Submit-sc-10l91w3-12').click();
+
+        cy.get('.Subscribe__Label-sc-1hdnlrs-1')
+            .should('have.text', 'Thanks for submitting the form.');
+    })
+});
