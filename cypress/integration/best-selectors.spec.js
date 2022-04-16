f81b7f2ff1852edaad56cb6c30936a84e185b4cd
breslavsky <breslavsky.anton@gmail.com>
2022-04-16 22:20:06 +0200

    Final best selectors

--- cypress/integration/best-selectors.spec.js
+++ cypress/integration/best-selectors.spec.js
@@ -1,25 +1,25 @@
 describe('Регистрация', () => {
 
-    it('открыть форму', () => {
+    it('зарегистрироваться', () => {
         cy.visit('http://localhost:3000/apps/registration.html');
-    });
-
-    it('заполнить форму', () => {
-        cy.get('#b1h7e4i8d3')
-            .type('Иванов');
+        cy.get('form.registration [name=first_name]').type('Иван');
+        cy.get('form.registration [name=last_name]').type('Иванов');
+        cy.get('form.registration [name=email]').type('test@test.com');
+        cy.get('form.registration [name=password]').type('qwerty');
+        cy.get('form.registration [type=submit]').click();
     });
 
 });
 
 describe('Вход', () => {
 
-    it('открыть форму', () => {
+    it('войти по email', () => {
         cy.visit('http://localhost:3000/apps/registration.html');
-    });
-
-    it('заполнить форму', () => {
-        cy.get('#b1h7e4i8d3')
-            .type('Иванов');
+        //cy.visit('http://localhost:3000/apps/registration.html?lang=en');
+        cy.get('form.login [name=email]').type('test@test.com');
+        cy.get('form.login [name=password]').type('qwerty');
+        cy.get('form.login [type=submit]').click();
+        //cy.contains('Войти').click();
     });
 
 });
