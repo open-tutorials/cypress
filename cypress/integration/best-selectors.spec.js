e8178adafeec6fe3ba22500f45424894469805a6
breslavsky <breslavsky.anton@gmail.com>
2022-12-19 23:42:19 +0100

    Add track

--- cypress/integration/best-selectors.spec.js
+++ cypress/integration/best-selectors.spec.js
@@ -2,11 +2,11 @@
 
 it('should do register user', () => {
     cy.visit('http://localhost:3000/apps/tesla.html');
-    cy.get('form.registration [name=first_name]').type('Иван');
-    cy.get('form.registration [name=last_name]').type('Иванов');
-    cy.get('form.registration [name=email]').type('test@test.com');
-    cy.get('form.registration [name=password]').type('qwerty');
-    cy.get('form.registration [type=submit]').click();
+    cy.get('form.registration input[name=first_name]').type('Иван');
+    cy.get('form.registration input[name=last_name]').type('Иванов');
+    cy.get('form.registration input[name=email]').type('test@test.com');
+    cy.get('form.registration input[name=password]').type('qwerty');
+    cy.get('form.registration button[type=submit]').click();
 
     cy.get('.main .success').should('have.text', 'Вы зарегистрированы');
 });
@@ -14,9 +14,9 @@ it('should do register user', () => {
 it('should do login user', () => {
     cy.visit('http://localhost:3000/apps/tesla.html');
     //cy.visit('http://localhost:3000/apps/tesla.html?lang=en');
-    cy.get('form.login [name=email]').type('test@test.com');
-    cy.get('form.login [name=password]').type('qwerty');
-    cy.get('form.login [type=submit]').click();
+    cy.get('form.login input[name=email]').type('test@test.com');
+    cy.get('form.login input[name=password]').type('qwerty');
+    cy.get('form.login button[type=submit]').click();
     //cy.contains('Войти').click();
 
     cy.get('.main .success').should('have.text', 'Вы вошли');
